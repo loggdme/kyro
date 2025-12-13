48017c9c23ec71584d2afa463472503bff10681a
DevTobias <github@tobiaskaerst.dev>
2025-12-13 22:06:55 +0100

    feat: filter function

--- functional.go
+++ functional.go
@@ -16,3 +16,13 @@ func FindFirst[T any](slice []T, predicate func(T) bool) *T {
 	}
 	return nil
 }
+
+func Filter[T any](slice []T, predicate func(T) bool) []T {
+	result := make([]T, 0, len(slice))
+	for _, item := range slice {
+		if predicate(item) {
+			result = append(result, item)
+		}
+	}
+	return result
+}
